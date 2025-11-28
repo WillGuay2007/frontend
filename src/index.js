28cf0220c72ed1c63fe86468404ad666a10c4278
William Guay <2430063@collegebart.ca>
2025-11-28 15:59:16 -0500

    e

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
+import './index.css';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/signin" element = {<Login/>}></Route>
+        <Route path = "/signup" element = {<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
